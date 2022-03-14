fba0dea4fa638451848f5f8f47a4790c2b11d602
Kim Yu Na <yunasun0920@gmail.com>
2022-03-14 09:15:39 +0900

    [Bronze IV] Title: 연세대학교, Time: 212 ms, Memory: 17468 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        if(n == 0)
+            System.out.println("YONSEI");
+        if(n == 1)
+            System.out.println("Leading the Way to the Future");
+    }
+}
