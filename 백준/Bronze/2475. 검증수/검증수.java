d9881b8e1b4c4ccfa19fe782cb2db07fef865f8e
Kim Yu Na <yunasun0920@gmail.com>
2022-03-17 08:53:05 +0900

    [Bronze V] Title: 검증수, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[5];
+        for(int i = 0; i < 5; i++)
+            arr[i] = sc.nextInt();
+        
+        int sum = 0;
+        for(int i = 0; i < 5; i++)
+            sum += Math.pow(arr[i], 2);
+        
+        System.out.println(sum % 10);
+    }
+}
