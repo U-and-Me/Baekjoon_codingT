d1d2fa0a2c8abe5c704d49f489e1c0769e75b6d0
Kim Yu Na <yunasun0920@gmail.com>
2022-03-21 20:55:44 +0900

    [Bronze V] Title: 카드 게임, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int sum = 0;
+        
+        for(int i = 0; i < 5; i++){
+            int score = sc.nextInt();
+            sum += score;
+        }
+        
+        System.out.println(sum);
+    }
+}
