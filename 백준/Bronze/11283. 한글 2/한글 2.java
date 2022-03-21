fbbacc52251fe21f006e517fa5c5003f37e1331d
Kim Yu Na <yunasun0920@gmail.com>
2022-03-21 21:17:41 +0900

    [Bronze V] Title: 한글 2, Time: 212 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        char text = sc.next().charAt(0);
+        
+        System.out.println((int)text - 44031);
+    }
+}
