fdd175d9db3ecafc740f641033a9b2bffab22a1c
Kim Yu Na <yunasun0920@gmail.com>
2022-05-10 08:15:07 +0900

    [level 2] Title: 프린터, Time: 0.76 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프린터/프린터.java
@@ -0,0 +1,30 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i : priorities)
+            queue.add(i);
+        
+        System.out.println(queue);
+        
+        while(!queue.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(queue.peek() == priorities[i]){
+                    if(i == location)
+                        return answer;
+                    
+                    answer++;
+                    queue.poll();
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}
